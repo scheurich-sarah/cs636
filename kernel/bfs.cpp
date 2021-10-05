ac2a56b957fde4d1fab16f5abc71ded3844665b4
Sarah C  Scheurich <sarah.scheurich@richmond.edu>
2021-10-05 07:21:27 -0400

    Add cout statements to observe effects on bfs_cpp.py. Not observable.

--- kernel/bfs.cpp
+++ kernel/bfs.cpp
@@ -5,15 +5,21 @@
 using std::cout;
 using std::endl;
 
+// this needs kernel.cpython
 
 void run_bfs(graph_t& g, vid_t root)
 {
     csr_t* csr = &g.csr;
     csr_t* csc = &g.csc;
     
+    cout << "csr.get_vcount = " << csr->get_vcount() << endl; 
+    cout << "csr.get_ecount = " << csr->get_ecount() << endl; 
+    cout << "degree of node 1 = " << csr->get_degree(1) << endl; 
     //TODO
+    // all the logic will go here
+    // just implement top down
 		
-    cout << "root = " << root << endl; 
+    cout << "in bfs.cpp: root = " << root << endl; 
     //print bfs tree here
     //i.e. how many vertex in each level
 }
