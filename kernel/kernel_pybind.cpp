0b790e431750dfb808cbea24005466243ae18fd3
Sarah C  Scheurich <sarah.scheurich@richmond.edu>
2021-10-05 07:18:35 -0400

    Added notes from class to file

--- kernel/kernel_pybind.cpp
+++ kernel/kernel_pybind.cpp
@@ -12,28 +12,35 @@ using std::endl;
 //#include "capsule_to_array.h"
 //#include "generated_pybind.h"
 
+// need to implement this
 void run_bfs(graph_t& graph, vid_t root);
 
+// names the output module m
 PYBIND11_MODULE(kernel, m) {
 
   py::class_<graph_t>(m, "graph_t")
     .def(py::init<>())
-     
+
+    // lambda functions
+    // how we expose functions on graph
+    // send arrays to c++
     .def("get_vcount", &graph_t::get_vcount)
     .def("get_edge_count", &graph_t::get_edge_count)
     
+    // should be able to call from graph
     .def("run_bfs",
         [](graph_t& graph, vid_t root) {
             run_bfs(graph, root);
         }
-    )
-    ;
+    );
     
     
   m.def("init_graph",
       [](py::array offset_csr, py::array nebrs_csr, py::array offset_csc, py::array nebrs_csc, int64_t flag, int64_t num_vcount) {
-           graph_t* graph =  new graph_t;
-           //cout<< offset_csr.shape(0) - 1<< "num_vcount"<< endl;
+
+           // * is just a dereference operator
+      	   graph_t* graph =  new graph_t;
+           cout<< offset_csr.shape(0) - 1<< "num_vcount"<< endl;
            graph->init(offset_csr.shape(0) - 1, nebrs_csr.itemsize(), 
                  offset_csr.request().ptr, nebrs_csr.request().ptr,
                  offset_csc.request().ptr, nebrs_csc.request().ptr, flag, num_vcount);
